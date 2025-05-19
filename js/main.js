82ce9a89c37a2c74b06ec3d0a04f2172adc1c984
JosePira20 <jdpirar@academia.usbbog.edu.co>
2025-05-18 20:08:25 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) {
+      slide.classList.add('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Inicia el carrusel automático cada 5 segundos
+setInterval(nextSlide, 5000);
